dc49ab20d631ec7ec723521d9329b997826484e5
JesusOcegueraPena <jo1138241@hotmail.com>
2020-02-27 18:23:11 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by JESUS OCEGUERA-PENA on 2/27/20.
+//  Copyright © 2020 JESUS OCEGUERA-PENA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
